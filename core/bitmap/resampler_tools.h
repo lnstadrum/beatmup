a9e4ef6de746bcee0503f01d7117e7a04327601a
lnstadrum from Pi <maxim.karpushin@gmail.com>
2020-03-22 16:20:36 +0000

    Debuggin BitmapResampler; adding Resample app

--- core/bitmap/resampler_tools.h
+++ core/bitmap/resampler_tools.h
@@ -10,40 +10,48 @@
 namespace Beatmup {
 namespace BitmapResamplingTools{
 
-    template<class in_t, class out_t> class CumulatingResampler {
+    template<class in_t, class out_t> class BoxResampler {
     public:
 
         /**
-            Resamples a rect from an input bitmap to a rect in an output bitmap
+            Resamples a rectangle from an input bitmap to a rectangle in an output bitmap applying a box filter
         */
         static void process(in_t in, out_t out, IntRectangle& src, IntRectangle& dst, const TaskThread& tt) {
             const int
-                WS = src.width() + 1, HS = src.height() + 1,
-                WD = dst.width() + 1, HD = dst.height() + 1;
+                srcW = src.width(), srcH = src.height(),
+                dstW = dst.width(), dstH = dst.height();
 
-            typename in_t::pixtype buffa;
-            int X0, Y0, X1, Y1 = src.a.y;
+            // dest image slice to process in the current thread
+            const int
+              sliceStart = tt.currentThread()       * dstH / tt.totalThreads(),
+              sliceStop  = (tt.currentThread() + 1) * dstH / tt.totalThreads();
 
-            for (int y = tt.currentThread(); y < HD; y += tt.totalThreads()) {
+            int x0, y0, x1, y1 = src.a.y + (sliceStart) * srcH / dstH;    // coordinates of source pixels box mapped to a given dest pixel
+
+            typename in_t::pixtype acc;
+            for (int y = sliceStart; y < sliceStop; ++y) {
                 out.goTo(dst.a.x, dst.a.y + y);
-                Y0 = Y1;
-                Y1 = src.a.y + (y + 1) * HS / HD;
-                X1 = src.a.x;
-                for (int x = 0; x < WD; ++x) {
-                    X0 = X1;
-                    X1 = src.a.x + (x + 1) * WS / WD;
-                    // source cycle
-                    buffa.zero();
-                    int Y = Y0;
+                y0 = y1;
+                y1 = src.a.y + (y + 1) * srcH / dstH;
+                x1 = src.a.x;
+                for (int x = 0; x < dstW; ++x) {
+                    x0 = x1;
+                    x1 = src.a.x + (x + 1) * srcW / dstW;
+
+                    // loop over the source area
+                    acc.zero();
+                    int y = y0;
                     do {
-                        in.goTo(X0, Y);
-                        int X = X0;
+                        in.goTo(x0, y);
+                        int x = x0;
                         do {
-                            buffa = buffa + in();
+                            acc = acc + in();
                             in++;
-                        } while (++X < X1);
-                    } while (++Y < Y1);
-                    out = buffa / std::max(1, ((X1 - X0)*(Y1 - Y0)));
+                        } while (++x < x1);
+                    } while (++y < y1);
+
+                    // write out
+                    out = acc / std::max(1, (x1 - x0)*(y1 - y0));
                     out++;
                 }
 
